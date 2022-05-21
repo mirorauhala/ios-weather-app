6dec93ffd691d9358d292c6a874fdaa4f8c206e6
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-05-21 16:04:22 +0300

    Add TabView

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var cities: [City] = [
+        City(id: 1, name: "Tampere", temp: 22),
+        City(id: 2, name: "Helsinki", temp: 10),
+        City(id: 3, name: "Oulu", temp: -10)
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        TabView {
+            WeatherView(cities: $cities)
+                .tabItem {
+                    Image(systemName: "sun.min")
+                    Text("Weather")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+        }
+    
+        
     }
 }
