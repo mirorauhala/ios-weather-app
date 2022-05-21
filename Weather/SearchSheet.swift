7de4276f29ee3ab081fd96bea413aa2a77178f3d
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-05-21 20:02:55 +0300

    Make sheet push new cities and to close itself

--- Weather/SearchSheet.swift
+++ Weather/SearchSheet.swift
@@ -8,18 +8,33 @@
 import SwiftUI
 
 struct SearchSheet: View {
-    @Binding var search: String
-    @Binding var searchResults: [City]
+    @ObservedObject var api: OpenWeatherMap
+    @Binding var isPresented: Bool
+    @State var query: String = ""
+    @State var searchResults: [Geocode] = []
     
     var body: some View {
         Form {
-            TextField("Search", text: $search)
+            TextField("Search", text: $query)
             Section {
                 List {
                     ForEach(searchResults) { city in
-                        Text(city.name).tag(city.id)
+                        Button("\(city.name), \(city.country)", action: {
+                            
+                            // when the button is tapped, get the city and push it to the api.cities list
+                            api.getWeather(lat: city.lat, lon: city.lon, format: api.unit) { responseCity in
+                                api.cities.insert(responseCity, at: 0)
+                                isPresented = false
+                            }
+                        }).tag(city.id)
                     }
                 }
+            }
+        }.onSubmit {
+            
+            // Submit the search request and show the cities from the response
+            api.search(query) { cities in
+                searchResults = cities
             }
         }
     }
