5ff0357c8ae1f710d74bb87a5c3bb4f8e1a36113
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-05-21 18:44:04 +0300

    Update CityCard to new API format

--- Weather/CityCard.swift
+++ Weather/CityCard.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 struct CityCard: View {
     @Binding var city: City
+    @Binding var unit: OpenWeatherMap.TemperatureFormat
     
     var body : some View {
         HStack {
             VStack(alignment: .leading) {
                 Spacer()
                 Text(city.name).font(.subheadline)
-                Text("\(city.temp)").font(.largeTitle)
+                Text("\(Int(city.main.temp))°\(OpenWeatherMap.getTempSymbol(unit))").font(.largeTitle)
                 Spacer()
             }
             Spacer()
             VStack(alignment: .trailing) {
-                AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/01d@2x.png"), scale: 1)
+                AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(city.weather[0].icon)@2x.png"), scale: 1)
                     .frame(width: 100, height: 100)
             }
         }
